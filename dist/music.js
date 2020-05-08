9f34a0aa663e6be4ab3e9937fb2db0d5ebea1915
Scalpel0507 <2323899856@qq.com>
2020-05-08 14:39:52 +0800

    Site updated: 2020-05-08 14:39:51

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    loop: one,
+	audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'url.mp3',
+        cover: 'cover.jpg'
+		
+    }]
+});
